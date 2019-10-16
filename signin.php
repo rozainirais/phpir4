e702fb83ef716f8f5b745906d67c8335ab6e1b24
rozainirais <56639858+rozainirais@users.noreply.github.com>
2019-10-16 21:35:28 +0800

    Create signin.php

---
+++ signin.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+<form action="welcome.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+User Id : <input type="text" name="username"><br>
+Password: <input type="password" name="password"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
