94bd371cb7fa7089a7a5ae4f7e3acaf0c5399532
rozainirais <56639858+rozainirais@users.noreply.github.com>
2019-10-17 01:41:14 +0800

    Create login.php

---
+++ login.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	
+		<h1>Sistem Pendaftaran Kelas Tambahan</h1>
+		<p>Login Pelajar</p>
+	</head>
+<body>
+
+<form action="dashboard.php" method="post">
+
+<label>E-mel: </label> <input type="text" name="email"class = "box"><br><br>
+<label>Kata Laluan: </label><input type="password" name="password"class = "box"><br><br>
+<input type="submit" value="Submit">
+</form>
+
+</body>
+</html>
