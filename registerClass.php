71b27f19a55ca0354bdce8e04bda6c1de256b3bb
rozainirais <56639858+rozainirais@users.noreply.github.com>
2019-10-17 01:42:40 +0800

    Create registerClass.php

---
+++ registerClass.php
@@ -0,0 +1,21 @@
+<html>
+<h2>Borang Pendaftaran Kelas</h2>
+
+<form action="registration_form2.php" method="POST">
+
+Class:
+ <select name="class">
+  <option value="php">Kelas Asas PHP</option>
+  <option value="python">Kelas Asas Python</option>
+  <option value="mysql">Kelas Asas Mysql</option>
+  <option value="html">Kelas Asas HTML5</option>
+</select> 
+
+<br>
+
+<input type="hidden" name="form_submitted" value="1" />
+
+<input type="submit" value="Submit">
+
+</form>
+</html>
